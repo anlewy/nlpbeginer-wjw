642be3f5e1c2f5ae9986a708b3e712f5e389b669
wujiawen <wujiawen.wy@bytedance.com>
2021-03-18 12:57:39 +0800

    task5 main

--- task5/main.py
+++ task5/main.py
@@ -1,7 +1,20 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 from task5.data import get_data_iter
+from task5.model import LSTM_LM
 
 
 BATCH_SIZE = 16
+EMBED_SIZE = 64
+MOMENTUM = 0.9
+LEARNING_RATE = 0.01
 
 
 train_iter, valid_iter, test_iter, TEXT = get_data_iter()
+pad_idx = TEXT.vocab.stoi(TEXT.pad_token)
+eos_idx = TEXT.vocab.stoi(TEXT.eos_token)
+
+model = LSTM_LM(len(TEXT.vocab), EMBED_SIZE)
+loss_func = nn.CrossEntropyLoss(ignore_index=pad_idx, reduction="sum")
+optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
