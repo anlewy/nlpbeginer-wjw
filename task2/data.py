871a82a282e030bc489c11e5044c948b1819929e
wujiawen <wujiawen.wy@bytedance.com>
2021-03-18 21:28:42 +0800

    task1

--- task2/data.py
+++ task2/data.py
@@ -1,26 +1,47 @@
 import torch
-
-from task1.feature_exaction import Tfidf
-from task1.load_data import load_testing_data, load_training_data
+from torchtext.legacy.data import Field, LabelField, Iterator, BucketIterator, TabularDataset
 
 
-def get_data_iter():
-    documents, y = load_training_data(0.1)
-    n = len(y)
-    train_num = n * 2 // 3
-    tfidfM = Tfidf()
+def get_data_iter(data_path="data", batch_size=32, device='cpu'):
+    TEXT = Field(batch_first=True, include_lengths=True, lower=True)
+    LABEL = LabelField(batch_first=True, include_lengths=True)
 
-    documents_train, y_train = documents[:train_num], y[:train_num]
-    X_train = tfidfM.fit_transform(documents_train)
-    X_train = torch.tensor(X_train)
-    y_train = torch.tensor(y_train)
-    data_train_iter = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_train, y_train), batch_size=10,
-                                                  shuffle=True)
-    documents_test, y_test = documents[train_num:], y[train_num:]
-    X_test = tfidfM.transform(documents_test)
-    X_test = torch.tensor(X_test)
-    y_test = torch.tensor(y_test)
-    data_test_iter = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X_test, y_test), batch_size=10,
-                                                 shuffle=True)
-    print("size of train_data:{}, size of test_data:{}".format(len(y_train), len(y_test)))
-    return data_train_iter, data_test_iter, tfidfM.feature_num, len(set(y))
+    fields = {
+        'Phrase': ('phrase', TEXT),
+        'Sentiment': ('sentiment', LABEL)
+    }
+
+    train_valid_data, test_data = TabularDataset.splits(
+        path=data_path,
+        train='train.tsv',
+        test='test.tsv',
+        format='tsv',
+        fields=fields
+    )
+
+    train_data, valid_data = train_valid_data.splits([0.8, 0.2])
+
+    TEXT.build_vocab(train_data)
+    LABEL.build_vocab(valid_data)
+
+    train_iter, valid_iter = BucketIterator.splits(
+        (train_data, valid_data),
+        batch_sizes=(batch_size, batch_size),
+        device=device,
+        sort_key=lambda e: len(e.phrase),
+        sort_within_batch=True,
+        repeat=False,
+        shuffle=True
+    )
+
+    test_iter = Iterator(
+        test_data,
+        batch_size=batch_size,
+        device=device,
+        sort=False,
+        sort_within_batch=False,
+        repeat=False,
+        shuffle=False
+    )
+
+    return train_iter, valid_iter, test_iter, TEXT, LABEL
---
+++ task2/main.py
@@ -0,0 +1,23 @@
+from task2.data import get_data_iter
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+DATA_PATH = 'data'
+BATCH_SIZE = 32
+EMBED_SIZE = 64
+HIDDEN_SIZE = 32
+DROPOUT_RATE = 0.3
+LAY_NUM = 2
+LEARNING_RATE = 0.01
+EPOCHS = 16
+DEVICE = 'cpu'
+
+#获取数据
+train_iter, valid_iter, test_iter, TEXT, LABEL = get_data_iter(data_path=DATA_PATH, batch_size=BATCH_SIZE, device=DEVICE)
+
+# 三要素
+loss_func = nn.CrossEntropyLoss()
+optimizer = optim.Adam()
+
+# 模型训练和模型评价
